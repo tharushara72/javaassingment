52530b2ac4fe0e8d719899e276e24e22c07b994e
tharushara72 <148558005+tharushara72@users.noreply.github.com>
2024-12-06 10:03:52 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person {
+    private String name;
+
+    // Constructor
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+}
+
+
